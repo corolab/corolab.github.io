ba2c5991e8654c4120e35008d4ae4700e3eeef0f
Bowin Zhang <zbwen406@yeah.net>
2023-10-20 21:43:39 +0100

    Apply grid layout to other images modules

--- js/masonry_init.js
+++ js/masonry_init.js
@@ -1,9 +1,27 @@
-var $grid = $('.phd-students').masonry({
+var $phdGrid = $('.phd-students').masonry({
     itemSelector: '.phd-student-item',
     horizontalOrder: true,
     initLayout: true,
 });
 
-$grid.imagesLoaded().progress( function() {
+var $alumniGrid = $('.alumni').masonry({
+    horizontalOrder: true,
+    initLayout: true,
+});
+
+var $visitingScholarGrid = $('.visiting-scholar').masonry({
+    horizontalOrder: true,
+    initLayout: true,
+});
+
+$phdGrid.imagesLoaded().progress( function() {
+    $grid.masonry('layout');
+});
+
+$alumniGrid.imagesLoaded().progress( function() {
+    $grid.masonry('layout');
+});
+
+$visitingScholarGrid.imagesLoaded().progress( function() {
     $grid.masonry('layout');
 });
